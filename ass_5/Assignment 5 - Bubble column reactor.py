ec0c0b4d5990a6ae8f68c64b94360b577c5d024d
Xikiee <oliverl.lohr@gmail.com>
2026-03-15 23:56:53 +0100

    assignment 5 code

--- ass_5/Assignment 5 - Bubble column reactor.py
+++ ass_5/Assignment 5 - Bubble column reactor.py
@@ -7,16 +7,19 @@ from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # function
 def funstep1 (z,t):
-    c1, c2 = z
+    c1, c2 = z          # where c1 = [CO_2] and c2 = [OH]
 
     #Henry_constant for electrolyte solution
-    H_L = ***
-    r1 = ***
-    dc1dt = ***
-    dc2dt = ***    
+    H_L = 3.3e-2
     
+    r1 = k1*c1* c2
+    co2_ref = H_L * 0.30 # since 30% of co2 in gas phase 
+    dc1dt = kla*(co2_ref - c1)-r1
+    dc2dt = -r1    
+
     return [dc1dt,dc2dt]
 
  
@@ -38,10 +41,11 @@ C0_NaOH = 0.04 #mol/l
 FCO2 = 70 #l/min
 
 # concentration of CO2 in gas phase --> assumption: ideal gas 
-C0_CO2 = *** #mol/l
+C0_CO2 = (0.3*101325)/(R*T)/1000 #mol/l                     ######################### change this weird as 
 
+print(C0_CO2)
 # physical properties
-ug = ***  #m/s
+ug = 6.6e-4  #m/s
 kla = 0.467*ug**0.82  #1/s
 
 sigma = 0.073 #N/m 
@@ -50,7 +54,7 @@ k1= 4  #l/(mol*s)
 
 
 # initial conditions
-init= [***, ***]
+init= [0, 0.04]                                                           
 t_span = np.linspace(0, t_Max) 
 
 # calculation
@@ -60,6 +64,24 @@ c2 = z[:, 1]
 
 # Plot
 plt.plot(t_span, c1, label='c1(t)')
+plt.xlabel("Time (s)", weight = "bold")
+plt.ylabel("[CO2] (mol/l)", weight = "bold")
+plt.title("Concentration of CO2 over Time")
+plt.minorticks_on()
+plt.grid(which= "major", linewidth = 1)
+plt.grid(which="minor", linewidth = 0.5)
+plt.xlim(0,t_Max)
+plt.ylim(0, c1[-1])
+plt.legend()
 plt.show()
 plt.plot(t_span, c2, label='c2(t)')
+plt.xlabel("Time (s)", weight = "bold")
+plt.ylabel("[OH-] (mol/l)", weight = "bold")
+plt.title("Concentration of OH- over Time")
+plt.minorticks_on()
+plt.grid(which= "major", linewidth = 1)
+plt.grid(which="minor", linewidth = 0.5)
+plt.xlim(0,t_Max)
+# plt.ylim(0,c2[-1])
+plt.legend()
 plt.show()
